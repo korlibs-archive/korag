00f645bc2ea0df377598a1dc296ce3aff7b11af0
soywiz <soywiz@gmail.com>
2017-10-19 21:25:39 +0200

    Updated to kotlin 1.2.0-beta-88

--- korag/common/src/main/kotlin/com/soywiz/korag/geom/Matrix3.kt
+++ korag/common/src/main/kotlin/com/soywiz/korag/geom/Matrix3.kt
@@ -1,6 +1,6 @@
 package com.soywiz.korag.geom
 
-import com.soywiz.korio.lang.JvmField
+import com.soywiz.korio.JvmField
 
 class Matrix3(@JvmField val `val`: FloatArray = floatArrayOf(
 	1f, 0f, 0f,
