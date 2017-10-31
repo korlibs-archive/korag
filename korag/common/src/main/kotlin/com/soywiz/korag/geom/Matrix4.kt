6a37eff7bbe2fa6003e144d8b4d26a2080b4dcae
soywiz <soywiz@gmail.com>
2017-10-31 03:07:27 +0100

    Additional Matrix4 methods

--- korag/common/src/main/kotlin/com/soywiz/korag/geom/Matrix4.kt
+++ korag/common/src/main/kotlin/com/soywiz/korag/geom/Matrix4.kt
@@ -64,12 +64,73 @@ class Matrix4(val data: FloatArray = floatArrayOf(
 		a1: Float, b1: Float, c1: Float, d1: Float,
 		a2: Float, b2: Float, c2: Float, d2: Float,
 		a3: Float, b3: Float, c3: Float, d3: Float
-	): Matrix4 {
+	): Matrix4 = this.apply {
 		setRow(0, a0, b0, c0, d0)
 		setRow(1, a1, b1, c1, d1)
 		setRow(2, a2, b2, c2, d2)
 		setRow(3, a3, b3, c3, d3)
-		return this
+	}
+
+	fun setToIdentity() = this.apply {
+		this.setTo(
+			1f, 0f, 0f, 0f,
+			0f, 1f, 0f, 0f,
+			0f, 0f, 1f, 0f,
+			0f, 0f, 0f, 1f
+		)
+	}
+
+	fun setToMultiply(l: Matrix4, r: Matrix4) {
+		multiply(this.data, l.data, r.data)
+	}
+
+	private fun multiply(out: FloatArray, a: FloatArray, b: FloatArray): FloatArray {
+		val a00 = a[0]
+		val a01 = a[1]
+		val a02 = a[2]
+		val a03 = a[3]
+		val a10 = a[4]
+		val a11 = a[5]
+		val a12 = a[6]
+		val a13 = a[7]
+		val a20 = a[8]
+		val a21 = a[9]
+		val a22 = a[10]
+		val a23 = a[11]
+		val a30 = a[12]
+		val a31 = a[13]
+		val a32 = a[14]
+		val a33 = a[15]
+
+		// Cache only the current line of the second matrix
+		var b0 = b[0]
+		var b1 = b[1]
+		var b2 = b[2]
+		var b3 = b[3]
+		out[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
+		out[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
+		out[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
+		out[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
+
+		b0 = b[4]; b1 = b[5]; b2 = b[6]; b3 = b[7]
+		out[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
+		out[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
+		out[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
+		out[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
+
+		b0 = b[8]; b1 = b[9]; b2 = b[10]; b3 = b[11]
+		out[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
+		out[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
+		out[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
+		out[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
+
+		b0 = b[12]; b1 = b[13]; b2 = b[14]; b3 = b[15]
+		out[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
+		out[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
+		out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
+		out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
+
+		return out
 	}
 
 	fun copyFrom(that: Matrix4): Matrix4 {
